8ac27134273b9d0656fb5d334c19b0b27dcf5a4a
Abel Cornejo <abelcornejo@overskull.pe>
2026-09-25 19:56:29 +0000

    fix:cambio del app v1.1

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('http');
 http.createServer((req, res) => {
   res.writeHead(200);
-  res.end('Hola desde el pipeline CI/CD - version 1\n');
+  res.end('Hola desde el pipeline CI/CD - version 1.1\n');
 }).listen(3001);
